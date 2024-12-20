90b8555a1f5c2447bb4f02216d55f4e7a8dafdb8
Aidan Thomas <aidanthomas@Aidans-Air.lan>
2024-12-20 00:21:24 -0600

    I played myself

--- Day20/day20.go
+++ Day20/day20.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
-	"slices"
 	"time"
 
 	aoc "github.com/MrPurpleDonut/aoc-functions"
@@ -75,6 +75,7 @@ func solve(start, end point, grid [][]rune) int {
 func populateCache(end point, grid [][]rune) map[cord]int {
 	cache := make(map[cord]int)
 	visited := aoc.MakeBoolBoard(len(grid), len(grid[0]))
+	visited[end.y][end.x] = true
 	s := size{len(grid), len(grid[0])}
 	queue := make([]point, 1)
 	queue[0] = end
@@ -98,44 +99,40 @@ func populateCache(end point, grid [][]rune) map[cord]int {
 
 }
 
-func solveWithCheat(start, end point, grid [][]rune, dir direction, move int) int {
-	visited := aoc.MakeBoolBoard(len(grid), len(grid[0]))
-	s := size{len(grid), len(grid[0])}
-	queue := make([]point, 1)
-	queue[0] = start
-	for len(queue) > 0 {
-		curPos := queue[0]
-		queue = queue[1:]
-		if curPos.x == end.x && curPos.y == end.y {
-			return curPos.cost
-		}
-		if curPos.cost == move {
-			n := point{curPos.x + 2*dir.dx, curPos.y + 2*dir.dy, curPos.cost + 2}
-			if n.x >= 0 && n.x < s.x && n.y >= 0 && n.y < s.y {
-				if visited[n.y][n.x] {
-					continue
-				}
-				if grid[n.y][n.x] != '.' {
-					continue
-				}
-				queue = append(queue, n)
-			}
-		} else {
-			for _, n := range getNeighbors(curPos, s) {
-				if visited[n.y][n.x] {
-					continue
-				}
-				if grid[n.y][n.x] != '.' {
-					continue
-				}
-				visited[n.y][n.x] = true
-				queue = append(queue, n)
-			}
-		}
-		slices.SortFunc(queue, func(a, b point) int { return a.cost - b.cost })
-
+func mattDistance(a, b cord) int {
+	val1 := a.x - b.x
+	val2 := a.y - b.y
+	if val1 < 0 {
+		val1 *= -1
 	}
-	return -1
+	if val2 < 0 {
+		val2 *= -1
+	}
+	return val1 + val2
+}
+
+func calculateCost(p1, p2, extra, total int) int {
+	if p1 <= p2 {
+		return math.MaxInt16
+	}
+	return total - p1 + p2 + extra
+}
+
+func getVal(dist, val, threshold int, cache map[cord]int) int {
+	count := 0
+	for k := range cache {
+		for j := range cache {
+			d := mattDistance(k, j)
+			if d == 1 || d > dist {
+				continue
+			}
+			if calculateCost(cache[k], cache[j], d, val)+threshold <= val {
+				count++
+			}
+
+		}
+	}
+	return count
 }
 
 func main() {
@@ -143,7 +140,6 @@ func main() {
 
 	grid, err := aoc.MakeMatrix(os.Args[1])
 	aoc.HandleError(err)
-	count := 0
 	var start, end point
 	for i, row := range grid {
 		for j, v := range row {
@@ -160,20 +156,8 @@ func main() {
 	}
 	val := solve(start, end, grid)
 	cache := populateCache(end, grid)
+	fmt.Println(getVal(2, val, 100, cache))
+	fmt.Println(getVal(20, val, 100, cache))
 
-	directions := []direction{
-		{-1, 0},
-		{0, 1},
-		{1, 0},
-		{0, -1},
-	}
-	for i := range val {
-		for _, d := range directions {
-			if v := solveWithCheat(start, end, grid, d, i); v > 0 && v+100 <= val {
-				count++
-			}
-		}
-	}
-	fmt.Println(count)
 	fmt.Println(time.Since(s))
 }
