f7b3d9d901f6e96ebddba82082fc07c8ef053244
Aidan Thomas <aidanthomas@Aidans-MacBook-Air.local>
2024-12-22 09:50:47 -0600

    half a second

--- Day22/day22.go
+++ Day22/day22.go
@@ -8,21 +8,19 @@ import (
 	aoc "github.com/MrPurpleDonut/aoc-functions"
 )
 
-var mod = 16777216
-
-func getPrices(price []int) map[pattern]int {
-	patterns := make(map[pattern]int)
+func getPrices(price []int, allPatterns *map[pattern]int) {
+	patterns := make(map[pattern]bool)
 	for i := 1; i < len(price)-4; i++ {
 		val := pattern{price[i] - price[i-1],
 			price[i+1] - price[i],
 			price[i+2] - price[i+1],
 			price[i+3] - price[i+2]}
 		if _, ok := patterns[val]; !ok {
-			patterns[val] = price[i+3]
+			(*allPatterns)[val] += price[i+3]
+			patterns[val] = true
 		}
 
 	}
-	return patterns
 }
 
 type pattern struct {
@@ -32,19 +30,20 @@ type pattern struct {
 	l int
 }
 
+var mod = 16777216 - 1
+
 func opperate(val, iterations int) (int, []int) {
 	res := make([]int, iterations+1)
 	res[0] = val % 10
 	for i := range iterations {
-		temp := val * 64
-		val ^= temp
-		val %= mod
-		temp = val / 32
-		val ^= temp
-		val %= mod
+		val ^= val * 64
+		val &= mod
+
+		val ^= val / 32
+		val &= mod
 
 		val ^= (val * 2048)
-		val %= mod
+		val &= mod
 		res[i+1] = val % 10
 	}
 	return val, res
@@ -52,27 +51,16 @@ func opperate(val, iterations int) (int, []int) {
 
 func findMax(prices [][]int) int {
 	max := 0
-	priceMaps := make([]map[pattern]int, len(prices))
-	for i, v := range prices {
-		priceMaps[i] = getPrices(v)
+	priceMap := make(map[pattern]int)
+	for _, v := range prices {
+		getPrices(v, &priceMap)
 	}
-	for i := -9; i <= 9; i++ {
-		for j := -9; j <= 9; j++ {
-			for k := -9; k <= 9; k++ {
-				for l := -9; l <= 9; l++ {
-					sum := 0
-					for _, p := range priceMaps {
-						if v, ok := p[pattern{i, j, k, l}]; ok {
-							sum += v
-						}
-					}
-					if sum > max {
-						max = sum
-					}
-				}
-			}
+	for _, v := range priceMap {
+		if v > max {
+			max = v
 		}
 	}
+
 	return max
 }
 
